4030a78da8aa134ad481f50c90ce7fa183408a99
eifal <syahrifal7@gmail.com>
2024-07-30 08:43:56 +0700

    $(call inherit-product, hardware/qcom-caf/common/common.mk)

--- kona.mk
+++ kona.mk
@@ -7,6 +7,9 @@
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
+# Add common definitions for Qualcomm
+$(call inherit-product, hardware/qcom-caf/common/common.mk)
+
 #Dolby
 $(call inherit-product, hardware/dolby/dolby.mk)
 
